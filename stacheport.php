7314b253af3453209771ca75cc2600a004d2bc20
seanyainkiranina <seanyainkiranina@yahoo.com>
2015-04-02 21:38:16 -0500

    Added License

--- stacheport.php
+++ stacheport.php
@@ -1,4 +1,15 @@
 <?php
+
+/*
+ * This file is part of stacheport
+ *
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ *
+ */
+
 class Stacheport
 {
         private $template='',$group_by=false,$group_collection='',$group_field='';
